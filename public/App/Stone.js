4772a573cfe4b9a7916a847377a1a940a9553831
skroneis <skroneis@yahoo.com>
2016-09-07 15:12:58 +0200

    init

---
+++ public/App/Stone.js
@@ -0,0 +1,26 @@
+﻿mainModule.controller("StoneController", function ($scope, viewModelHelper, $http) {
+    $scope.Sepp = "Sepp Forcher";
+
+    var initialize = function () {
+        console.log("initialize");
+        $scope.getInfos();
+    }
+
+    $scope.init = function () {
+        console.log("init");
+        initialize();
+    };
+
+$scope.getInfos = function () {
+        //alert("getInfos");
+        console.log("getInfos");
+        $scope.isLoading = true;
+        $http.defaults.headers.common["RequestVerificationToken"] = $scope.token;
+        return $http.get(MyApp.rootPath + 'api/getInfos', null).then(function (response) {
+            console.log(response.data);
+            $scope.infos = response.data;
+            $scope.isLoading = false;
+        });
+        // $scope.isLoading = false;
+    };
+});
